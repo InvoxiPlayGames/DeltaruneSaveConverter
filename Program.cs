522cc0b682d852d8d67fce56720ddbf592e1758f
InvoxiPlayGames <webmaster@invoxiplaygames.uk>
2021-09-21 08:28:36 +0100

    create INI files from savegames (BuildINI command)

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace DeltaruneSaveConverter
             Console.WriteLine("PackSAV: packs a SAV file with files from path1 into path2 - supports UNDERTALE");
             Console.WriteLine("ConvertFileFromConsole: converts the save file at path1 into PC format and saves it at path2");
             Console.WriteLine("ConvertFileFromPC: converts the save file at path1 into console format and saves it at path2");
+            Console.WriteLine("BuildINI: builds a dr.ini file from a folder containing PC-format save files");
             Console.WriteLine();
             Console.WriteLine("example:");
             Console.WriteLine("DeltaruneSaveConverter ConvertFromConsole deltarune_ch1.sav ./pcsave/");
@@ -51,6 +52,9 @@ namespace DeltaruneSaveConverter
                     break;
                 case "convertfilefrompc":
                     ConvertFileFromPC(args[1], args[2]);
+                    break;
+                case "buildini":
+                    BuildINI(args[1], args[2]);
                     break;
                 default:
                     PrintUsage();
@@ -209,6 +213,81 @@ namespace DeltaruneSaveConverter
             DeltaruneCh1 save = new();
             save.ReadFromPCFile(pcPath);
             save.WriteToConsoleFile(consolePath);
+        }
+
+        static void BuildINI(string pcPath, string iniPath)
+        {
+            Console.WriteLine("Opening INI file...");
+            IniFile ini = new(iniPath);
+            Console.WriteLine("Reading PC save files...");
+            int status = 0;
+            foreach (string file in Directory.GetFiles(pcPath))
+            {
+                string filename = Path.GetFileName(file);
+                if (filename.StartsWith("filech1")) // assume this is a PC save file from Chapter 1
+                {
+                    try
+                    {
+                        Console.Write($"Reading {filename}... ");
+                        DeltaruneCh1 save = new();
+                        save.ReadFromPCFile(file);
+                        Console.WriteLine("done!");
+                        Console.Write($"Writing \"{save.truename}\" chapter 1 save to INI... ");
+                        int saveid = int.Parse(filename.Replace("filech1_", ""));
+                        if (saveid >= 3 && saveid < 6)
+                            status = 1; // game complete, set console-only STATUS variable
+                        ini.Write("Name", save.truename, $"G{saveid}");
+                        ini.Write("Level", $"\"{save.lv}\"", $"G{saveid}");
+                        ini.Write("Love", $"\"{save.llv}\"", $"G{saveid}");
+                        ini.Write("Time", $"\"{save.time}\"", $"G{saveid}");
+                        ini.Write("Date", $"\"0\"", $"G{saveid}"); // TODO: Get valid date value
+                        ini.Write("Room", $"\"{save.currentroom}\"", $"G{saveid}");
+                        ini.Write("InitLang", $"\"{save.flag[912]}\"", $"G{saveid}");
+                        int uraboss = 0;
+                        if (save.flag[241] == 6) uraboss = 1;
+                        if (save.flag[241] == 7) uraboss = 2;
+                        ini.Write("UraBoss", $"\"{uraboss}\"", $"G{saveid}");
+                        Console.WriteLine("done!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"error: {ex.Message}");
+                        return;
+                    }
+                }
+                else if (filename.StartsWith("filech2")) // assume this is a PC save file from Chapter 2
+                {
+                    try
+                    {
+                        Console.Write($"Reading {filename}... ");
+                        DeltaruneCh2 save = new();
+                        save.ReadFromPCFile(file);
+                        Console.WriteLine("done!");
+                        Console.Write($"Writing \"{save.truename}\" chapter 2 save to INI... ");
+                        int saveid = int.Parse(filename.Replace("filech2_", ""));
+                        if (saveid >= 3 && saveid < 6)
+                            status = 1; // game complete, set console-only STATUS variable
+                        ini.Write("Name", save.truename, $"G_2_{saveid}");
+                        ini.Write("Level", $"\"{save.lv}\"", $"G_2_{saveid}");
+                        ini.Write("Love", $"\"{save.llv}\"", $"G_2_{saveid}");
+                        ini.Write("Time", $"\"{save.time}\"", $"G_2_{saveid}");
+                        ini.Write("Date", $"\"0\"", $"G_2_{saveid}"); // TODO: Get valid date value
+                        ini.Write("Room", $"\"{save.currentroom}\"", $"G_2_{saveid}");
+                        ini.Write("InitLang", $"\"{save.flag[912]}\"", $"G_2_{saveid}");
+                        int uraboss = 0;
+                        if (save.flag[571] == 1) uraboss = 1;
+                        if (save.flag[571] == 2) uraboss = 2;
+                        ini.Write("UraBoss", $"\"{uraboss}\"", $"G_2_{saveid}");
+                        Console.WriteLine("done!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"error: {ex.Message}");
+                        return;
+                    }
+                }
+            }
+            ini.Write("STATUS", $"\"{status}\"", "STATUS");
         }
     }
 }
